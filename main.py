dec00bc40aeaf62a39896933fa7fe743f8e5111e
sunilcsanthosh <sunilcsanthosh@gmail.com>
2023-06-04 10:15:33 +0400

    Updates

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from flask import Flask,render_template # here "Flask" is a class
 import pandas as pd
 app = Flask("__name__")  # here "app" is website object
 
-stations = pd.read_csv("data_small/stations.txt",skiprows=20)
+stations = pd.read_csv("data_small/stations.txt",skiprows=17)
+stations =stations [["STAID","STANAME                                 "]]
 
 @app.route("/")
 def home():
-    return render_template("home.html",data =stations)
+    return render_template("home.html",data =stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def about(station,date):
-    path ="data_small/TG_STAID"
     filename = "data_small/TG_STAID" + str(station).zfill(6)+ ".txt"
     df =pd.read_csv(filename,skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df['    DATE']=="1860-01-05"]['   TG'].squeeze()/10
